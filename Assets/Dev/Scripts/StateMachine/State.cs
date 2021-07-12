8355a08ca73fab7fbd842ae7525314f4b88d2ee8
kabun tyou <745642803@qq.com>
2021-07-12 16:11:58 +0800

    state machine

---
+++ Assets/Dev/Scripts/StateMachine/State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    public virtual bool OnInit()
+    {
+        return false;
+    }
+    public virtual bool OnEnter()
+    {
+        return false;
+    }
+    public virtual bool OnExit()
+    {
+        return false;
+    }
+    public virtual bool OnCondition()
+    {
+        return true;
+    }
+    public virtual bool OnDispose()
+    {
+        return false;
+    }
+}
---
+++ Assets/Dev/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : State
+{
+    public Dictionary<int, State> stateDict = new Dictionary<int, State>();
+
+    public State current;
+    public int defaultStateKey = -1;
+
+    public override bool OnInit()
+    {
+        if (current == null)
+        {
+            if (stateDict.TryGetValue(defaultStateKey, out current))
+            {
+                return current.OnInit();
+            }
+            return base.OnInit();
+        }
+        return current.OnInit();
+    }
+    public override bool OnEnter()
+    {
+        if (current == null)
+        {
+            if (stateDict.TryGetValue(defaultStateKey, out current))
+            {
+                return current.OnEnter();
+            }
+            return base.OnInit();
+        }
+        return current.OnEnter();
+    }
+    public override bool OnExit()
+    {
+        if (current == null)
+        {
+            return base.OnExit();
+        }
+        return current.OnExit();
+    }
+    public override bool OnCondition()
+    {
+        if (current == null)
+        {
+            return base.OnCondition();
+        }
+        return current.OnCondition();
+    }
+    public override bool OnDispose()
+    {
+        if (current == null)
+        {
+            return base.OnDispose();
+        }
+        return current.OnDispose();
+    }
+}
