1f361e32bd88f9f3dc0dbc6ba079dbbd3445b72b
kabun tyou <745642803@qq.com>
2021-08-19 10:22:20 +0800

    pool

---
+++ Assets/Dev/Scripts/Base/Pool/ListPool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListPool<T>
+{
+    private static readonly ObjectPool<List<T>> s_ListPool = new ObjectPool<List<T>>(null, Clear);
+
+    static void Clear(List<T> list)
+    {
+        list.Clear();
+    }
+
+    public static List<T> Get()
+    {
+        return s_ListPool.Get();
+    }
+
+    public static bool Release(List<T> list)
+    {
+        return s_ListPool.Release(list);
+    }
+}
+    
---
+++ Assets/Dev/Scripts/Base/Pool/ObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class ObjectPool<T> where T : new()
+{
+    private int count = 0;
+    private Stack<T> m_Stack = new Stack<T>();
+
+    private UnityAction<T> OnGet;
+    private UnityAction<T> OnRelease;
+
+    public ObjectPool(UnityAction<T> OnGet, UnityAction<T> OnRelease)
+    {
+        this.OnGet = OnGet;
+        this.OnRelease = OnRelease;
+    }    
+
+    public T Get()
+    {
+        T obj;
+
+        if (m_Stack.Count <= 0)
+        {
+            count++;
+            obj = new T();
+        }
+        else
+        {
+            obj = m_Stack.Pop();
+        }
+
+        if (OnGet != null)
+        {
+            OnGet(obj);
+        }
+
+        return obj;
+    }
+
+    public bool Release(T obj)
+    {
+        if (m_Stack.Count > 0 && ReferenceEquals(m_Stack.Peek(), obj))
+            return false;
+
+        if (OnRelease != null)
+            OnRelease(obj);
+
+        m_Stack.Push(obj);
+        return true;
+    }
+}
