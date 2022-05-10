39d38ed544175af1c28199af138a6d36f2f9c3c5
Lobaerk <1197898236@qq.com>
2022-05-10 21:25:52 +0800

    gal
    gal

---
+++ Assets/Dev/Scripts/GalScene/GalMono.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GalMono : MonoBehaviour
+{
+    public List<MonoBehaviour> monoList;
+    private static GalMono _instance;
+    public static GalMono Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject("GalMono");
+                DontDestroyOnLoad(obj);
+                _instance =  obj.AddComponent<GalMono>();
+            }
+            return _instance;
+        }
+    }
+
+    public UnityAction OnInitComplete;
+    public void Init()
+    {
+        monoList = new List<MonoBehaviour>();
+
+        if (OnInitComplete != null)
+        {
+            OnInitComplete.Invoke();
+        }
+    }
+    /// <summary>
+    /// 游戏运行执行
+    /// </summary>
+    [RuntimeInitializeOnLoadMethod]
+    public static void OnGameStarRun()
+    {
+        
+        GalMono.Instance.Init();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadScene()
+    {
+
+    }
+}
---
+++ Assets/Dev/Scripts/GalScene/GalSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalScenePool
+{
+    public GalScene firstScene;
+    public GalScene lastScene;
+
+    public void Add(GalScene galScene)
+    {
+
+    }
+
+    public void Delete(GalScene galScene)
+    {
+
+    }
+
+    public void Clear()
+    {
+        firstScene = null;
+        lastScene = null;
+    }
+}
+public class GalSceneManager
+{
+    
+}
