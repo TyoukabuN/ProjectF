7978078c901499a610d4dd848af00cd97f1a58d4
Lobaerk <1197898236@qq.com>
2022-05-19 11:51:09 +0800

    屏了测试

--- Assets/Dev/Scripts/GalScene/GalMono.cs
+++ Assets/Dev/Scripts/GalScene/GalMono.cs
@@ -58,32 +58,53 @@ public class GalMono : MonoSingleton<GalMono>
     {
 
     }
+    #region 测试的部分
+    //private void OnGUI()
+    //{
+    //    if (GUILayout.Button("点击load图片场景"))
+    //    {
+    //        GalSceneManager.instance.LoadScene("scene1");
+    //    }
+    //    if (GUILayout.Button("查看当前载入场景"))
+    //    {
+    //        scenes = "";
+    //        GalScene node = GalSceneManager.instance.galScenePool.firstScene;
+    //        while (node != null)
+    //        {
+    //            scenes = scenes +"\n" + node.name;
+    //            node = node.nextScene;
+    //        }
+    //    }
 
-    private void OnGUI()
-    {
-        if (GUILayout.Button("点击load图片场景"))
-        {
-            GalSceneManager.instance.LoadScene("scene1");
-        }
-        if (GUILayout.Button("查看当前载入场景"))
-        {
-            scenes = "";
-            GalScene node = GalSceneManager.instance.galScenePool.firstScene;
-            while (node != null)
-            {
-                scenes = scenes +"\n" + node.name;
-                node = node.nextScene;
-            }
-        }
+    //    if (GUILayout.Button("切换场景"))
+    //    {
+    //        //string name = switchb ?"scene1":"scene2";
+    //        //GalSceneManager.instance.SwitchScene(name);
+    //        //switchb = !switchb;
+    //        GalSceneManager.instance.SwitchScene();
 
-        if (GUILayout.Button("切换场景"))
-        {
-            //string name = switchb ?"scene1":"scene2";
-            //GalSceneManager.instance.SwitchScene(name);
-            //switchb = !switchb;
-            GalSceneManager.instance.SwitchScene();
+    //    }
+    //    GUILayout.Label("<color=red><size=30>"+scenes+"</size></color>");
 
-        }
-        GUILayout.Label("<color=red><size=30>"+scenes+"</size></color>");
-    }
+    //    //if (GUILayout.Button("charu"))
+    //    //{
+    //    //    int[] arr = { 5, 8, 6, 4, 2, 1, 9, 7 };
+    //    //    for (int i = 1; i < arr.Length; i++)
+    //    //    {
+    //    //        int nextValue = arr[i];
+    //    //        int index = i - 1;
+    //    //        while (index >= 0 && arr[index] > nextValue)
+    //    //        {
+    //    //            arr[index + 1] = arr[index];
+    //    //            arr[index] = nextValue;
+    //    //            index--;
+    //    //        }
+    //    //    }
+    //    //    foreach (var item in arr)
+    //    //    {
+    //    //        Debug.Log(item);
+    //    //    }
+    //    //}
+    //}
+    #endregion
 }
