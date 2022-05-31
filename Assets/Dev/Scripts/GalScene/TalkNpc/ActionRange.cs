bd234e4af8eb273f53f8b1dacebc8492d832713c
Lobaerk <1197898236@qq.com>
2022-05-31 11:19:30 +0800

    检测

---
+++ Assets/Dev/Scripts/GalScene/TalkNpc/ActionRange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionRange : MonoBehaviour
+{
+    public float range;
+
+    public List<GameObject> rangeNpcs;
+    Collider[] colliders;
+    Dictionary<Collider, bool> rangeDic = new Dictionary<Collider, bool>();
+    Vector3 startPoint;
+    private void Update()
+    {
+        startPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z + range);
+        colliders = Physics.OverlapSphere(startPoint, range, LayerMask.GetMask("NPC"));
+        
+        foreach (var item in colliders)
+        {
+            item.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            InRangeEvent ire = item.TryGetComponent<InRangeEvent>();
+            ire.isIn = true;
+            ire.isDo = true;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(startPoint, range);
+    }
+}
---
+++ Assets/Dev/Scripts/GalScene/TalkNpc/InRangeEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InRangeEvent : MonoBehaviour
+{
+    public UnityAction onStart;
+    public UnityAction onUpdate;
+    public UnityAction onExit;
+
+    public bool isIn;
+    public bool isDo;
+    public bool isExit;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isIn)
+        {
+            if (onStart != null)
+            {
+                onStart.Invoke();
+            }
+            isIn = false;
+        }
+        if (isDo)
+        {
+            if (onUpdate != null)
+            {
+                onUpdate.Invoke();
+            }
+        }
+        if (isExit)
+        {
+            if (onExit != null)
+            {
+                onExit.Invoke();
+            }
+            isExit = false;
+        }
+
+    }
+}
